51d940323965b1efa70c9705dd4f4d9b4a4e0601
RCArenas <ricardo.calderon@rcarenas.es>
2025-07-26 21:15:05 +0200

    Add auth scaffolding and fix guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,7 +8,7 @@
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
-            {{ $slot ?? '' }}
+            @yield('content')
         </div>
     </body>
 </html>
